73cece69213b6db9a65bc17a8070eba1a0a5970b
jonas <jonas@titan.(none)>
2010-09-22 11:41:58 +0200

    smart scroller

---
+++ public/scroller.js
@@ -0,0 +1,19 @@
+old_offset = $(window).scrollTop();
+function scroll() {
+  if(old_offset >= 0) {// scrolling
+    window.scrollBy(0,1)
+    if((new_offset = $(window).scrollTop()) == old_offset) // wir sind unten
+      old_offset = -500;
+    else
+      old_offset = new_offset
+    
+  } else {
+    if(old_offset == -200) {
+      window.scrollTo(0, 0)
+    }
+    old_offset++;
+  }
+  $("#info").text(old_offset)
+    
+}
+window.setInterval(scroll, 2)
