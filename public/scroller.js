5e84456a362f8f986a1e8cd636088c45fd13c3d6
jonas <jonas@titan.(none)>
2010-09-22 11:44:38 +0200

    longer pause

--- public/scroller.js
+++ public/scroller.js
@@ -1,14 +1,15 @@
-old_offset = $(window).scrollTop();
+old_offset = -600;
+
 function scroll() {
   if(old_offset >= 0) {// scrolling
     window.scrollBy(0,1)
     if((new_offset = $(window).scrollTop()) == old_offset) // wir sind unten
-      old_offset = -500;
+      old_offset = -600;
     else
       old_offset = new_offset
     
   } else {
-    if(old_offset == -200) {
+    if(old_offset == -300) {
       window.scrollTo(0, 0)
     }
     old_offset++;
