4bebd9adf9eab1af993f7300cd2b3ddb4aed180d
Jonas Schneider <mail@jonasschneider.com>
2011-09-21 16:27:44 +0200

    remove debug cruft

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -3,40 +3,35 @@ require 'parser'
 
 class Fichte::Fetcher
   def self.run!
-    if ENV["LOCAL"]
-      self.extract(File.read("subst_001.htm"))
-    else
+    base = 'https://www.fichteportfolio.de/anzeige/'
+    next_page = 'subst_001.htm'
+    done = %w()
 
-      base = 'https://www.fichteportfolio.de/anzeige/'
-      next_page = 'subst_001.htm'
-      done = %w()
+    changes = [] 
+    while !next_page.nil?
+      puts "fetching #{next_page}"
+      url = URI.parse base + next_page
 
-      changes = [] 
-      while !next_page.nil?
-        puts "fetching #{next_page}"
-        url = URI.parse base + next_page
+      done << next_page
+      next_page = nil
+      
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.open_timeout = 5
+      http.read_timeout = 10
 
-        done << next_page
-        next_page = nil
-        
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        http.open_timeout = 5
-        http.read_timeout = 10
+      request = Net::HTTP::Get.new(url.path)
+      response = http.request(request)
+      
+      p = Fichte::Parser.new response.body
+      
+      next_page = p.next_page_name unless done.include? p.next_page_name
+      
+      changes << p.changes
 
-        request = Net::HTTP::Get.new(url.path)
-        response = http.request(request)
-        
-        p = Fichte::Parser.new response.body
-        
-        next_page = p.next_page_name unless done.include? p.next_page_name
-        
-        changes << p.changes
-
-      end
-
-      changes.flatten
     end
+
+    changes.flatten
   end
 end
