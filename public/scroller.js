74177ad6a847810c441faa427005f0893bb7c60b
Jonas Schneider <mail@jonasschneider.com>
2011-12-02 15:52:21 +0100

    fix the scroller

--- public/scroller.js
+++ public/scroller.js
@@ -1,18 +1,23 @@
-old_offset = -600;
+old_offset = -100;
 resume_timer = null;
 scroll_interval = null;
+runs = 0;
 
 function scroll() {
-  if(old_offset >= 0) {// scrolling
-    window.scrollBy(0,1)
-    if((new_offset = $(window).scrollTop()) == old_offset) // wir sind unten
-      old_offset = -600;
+  if(old_offset >= 0) { // scrolling
+    new_offset = old_offset + 1
+    $("#container").css("top", "-"+new_offset)
+    
+    if(($("#container").height() - $(window).height() + 100) <= old_offset) // wir sind unten
+      old_offset = -600
     else
       old_offset = new_offset
-    
-  } else {
+  } else { // done scrolling
     if(old_offset == -300) {
-      window.scrollTo(0, 0)
+      runs++
+      if(runs > 5)
+        location.reload()
+      $("#container").css("top", "0")
     }
     old_offset++;
   }
@@ -24,13 +29,13 @@ window.onmousemove = function() {
     scroll_interval = null;
   }
   if(!resume_timer)
-    resume_timer = window.setTimeout(start, 10000)
+    resume_timer = window.setTimeout(start, 3000)
 }
 
 function start() {
   resume_timer = null;
+  $("#container").css("position", "relative")
   if(!scroll_interval)
-    scroll_interval = window.setInterval(scroll, 2)
+    scroll_interval = window.setInterval(scroll, 20)
 }
-
-window.setTimeout(start, 10000)
+start()
